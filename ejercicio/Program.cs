6d45e99cee242c586f119b72e0e7d8925169407a
Maryi corea <maryicorea3@gmail.com>
2025-08-04 10:55:55 -0600

    Add files via upload

--- ejercicio/Program.cs
+++ ejercicio/Program.cs
@@ -10,6 +10,25 @@ namespace ejercicio
     {
         static void Main(string[] args)
         {
+            string nombreEmpleado;
+            int horasTrabajadas;
+            double costoHora;
+            double totalDevengado;
+
+            Console.WriteLine ("ingrese su nombre ");
+            Console.WriteLine("Costo por hora trabajada : ");
+            horasTrabajadas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Cantidad de horas trabajadas esta semana: ");
+            costoHora = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("==== Boleta semanal ====");
+            Console.WriteLine("Costo por hora trabajada: " + costoHora);
+            Console.WriteLine($"total de horas trabajadas es: {horasTrabajadas}");
+           
+
+            totalDevengado = costoHora * horasTrabajadas;
+            Console.WriteLine($"el total devengado correspondiente es de:L." + totalDevengado.ToString());
+
         }
     }
 }
